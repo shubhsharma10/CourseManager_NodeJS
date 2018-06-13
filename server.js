754250b46f4193bef2036d584170056d4b4f8091
Babubhai <shubhsharma10@gmail.com>
2018-06-13 06:05:03 -0400

    Added session and body parser

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
-const PORT = process.env.PORT || 4000;
+var bodyParser = require('body-parser');
+var session = require('express-session');
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 4000;
 mongoose.connect('mongodb://test1234:test1234@ds259070.mlab.com:59070/cs5610-summer1-ssharma');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}));
 
 app.get('/', function (req, res) {
     res.send('Hello World, This is Shubham Sharma')
