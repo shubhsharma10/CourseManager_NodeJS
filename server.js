c8070209579878a36b6635377a01b1b0a10620d0
Babubhai <shubhsharma10@gmail.com>
2018-06-13 00:03:27 -0400

    Updated for heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const PORT = process.env.PORT;
+
 var app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World, This is Shubham Sharma')
 });
 
-app.listen(3000);
+app.listen(PORT);
