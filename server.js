502a02328fa45f45d6ffaa5bc1ec58415096211a
Babubhai <shubhsharma10@gmail.com>
2018-06-13 00:48:25 -0400

    Added user schema,model,service

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://test1234:test1234@ds259070.mlab.com:59070/cs5610-summer1-ssharma');
 
 var app = express();
 
@@ -7,4 +9,14 @@ app.get('/', function (req, res) {
     res.send('Hello World, This is Shubham Sharma')
 });
 
+// var userModel = require('./models/user/user.model.server');
+// userModel.createUser({
+//     username: 'bob',
+//     password: 'bob'
+// });
+
+// userModel.findAllUsers()
+//     .then(function(users){
+//     console.log(users);
+// });
 app.listen(PORT);
