ecd20d8382d3f44b15f503065ac6834fcf97b128
Babubhai <shubhsharma10@gmail.com>
2018-06-14 10:45:59 -0400

    Added CORS support

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.use(session({
     resave: false,
     saveUninitialized: true,
