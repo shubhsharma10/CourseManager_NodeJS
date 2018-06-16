e5cb0d911e1c31fd0e6e5ff001fbbf9c7a1cf270
Babubhai <shubhsharma10@gmail.com>
2018-06-15 22:14:16 -0400

    Added update and delete section methods

--- models/section/section.model.server.js
+++ models/section/section.model.server.js
@@ -6,6 +6,26 @@ function createSection(section) {
     return sectionModel.create(section);
 }
 
+function findSectionById(sectionId) {
+    return sectionModel.findById(sectionId);
+}
+
+function updateSection(sectionId,section) {
+    return sectionModel.findByIdAndUpdate(sectionId,section,{new:true})
+        .exec()
+        .then(function(updatedSection){
+            return updatedSection;
+        });
+}
+
+function deleteSection(sectionId) {
+    return sectionModel.findById(sectionId)
+        .exec()
+        .then(function (section) {
+            return section.remove();
+        });
+}
+
 function findSectionsForCourse(courseId) {
     return sectionModel.find({courseId:courseId});
 }
@@ -26,5 +46,8 @@ module.exports = {
     createSection: createSection,
     findSectionsForCourse: findSectionsForCourse,
     decrementSectionSeat: decrementSectionSeat,
-    incrementSectionSeat: incrementSectionSeat
+    incrementSectionSeat: incrementSectionSeat,
+    findSectionById: findSectionById,
+    updateSection: updateSection,
+    deleteSection: deleteSection
 };
--- services/section.service.server.js
+++ services/section.service.server.js
@@ -1,6 +1,9 @@
 module.exports = function (app) {
     app.post('/api/course/:courseId/section',createSection);
     app.get('/api/course/:courseId/section',findSectionsForCourse);
+    app.get('/api/section/:sectionId',findSectionById);
+    app.put('/api/section/:sectionId',updateSection);
+    app.delete('/api/section/:sectionId',deleteSection);
     // app.post('/api/enroll/section/:sectionId', enrollStudentInSection);
     // app.get('/api/student/section', findSectionsForStudent);
     // app.get('/api/section/:sectionId/student', findStudentsForSection);
@@ -8,6 +11,36 @@ module.exports = function (app) {
 
     var sectionModel = require('../models/section/section.model.server');
 
+    function findSectionById(req, res) {
+        var id = req.params['sectionId'];
+        sectionModel.findSectionById(id)
+            .then(function (section) {
+                res.json(section);
+            })
+    }
+
+    function updateSection(req,res) {
+        var sectionId = req.params['sectionId'];
+        var newSection = req.body;
+        sectionModel.updateSection(sectionId,newSection)
+            .then(function (updatedSection) {
+                res.json(updatedSection);
+            })
+            .catch(function(error){
+                res.sendStatus(500).send(error);
+            });
+    }
+
+    function deleteSection(req,res) {
+        var sectionId = req.params['sectionId'];
+        sectionModel.deleteSection(sectionId)
+            .then(function () {
+                res.sendStatus(200);
+            })
+            .catch(function(error){
+                res.sendStatus(500).send(error);
+            });
+    }
 
     function findSectionsForCourse(req, res) {
         var courseId = req.params['courseId'];
